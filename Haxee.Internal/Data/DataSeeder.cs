f815f1411eaecc5f25d1288431df2c804f27663a
Katka Marcincakova <katulik137@gmail.com>
2023-02-06 21:56:22 +0100

    added identity

---
+++ Haxee.Internal/Data/DataSeeder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Haxee.Internal.Data
+{
+    public class DataSeeder
+    {
+        public static async Task Seed(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            await userManager.CreateAsync(new User
+            {
+                Name = "Jozef Veduci",
+                UserType = Entities.Enums.UserType.Instructor,
+                SuperInstructor = true,
+                Email = "superjozef@lstme.sk",
+                EmailConfirmed = true
+            }, "jozef123");
+
+            await userManager.CreateAsync(new User
+            {
+                Name = "Jozef Instruktor",
+                UserType = Entities.Enums.UserType.Instructor,
+                Email = "jozef@lstme.sk",
+                EmailConfirmed = true
+            }, "jozef123");
+
+            await userManager.CreateAsync(new User
+            {
+                Name = "Jozef Ucastnik",
+                UserType = Entities.Enums.UserType.Kid,
+                Email = "ucastnik@lstme.sk",
+                EmailConfirmed = true
+            }, "jozef123");
+        }
+    }
+}
--- Haxee.Web/Program.cs
+++ Haxee.Web/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Haxee.Web
@@ -11,20 +12,32 @@ namespace Haxee.Web
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
+
             builder.Services.AddDbContext<DataContext>(options => 
             {
                 options.UseInMemoryDatabase("MainDB");
             });
 
+            builder.Services.AddIdentity<User, IdentityRole>(options =>
+            {
+            })
+                .AddEntityFrameworkStores<DataContext>()
+                .AddDefaultTokenProviders();
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            using (var scope = app.Services.CreateScope())
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                DataSeeder.Seed(scope.ServiceProvider).GetAwaiter().GetResult();
             }
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
 
             app.UseHttpsRedirection();
 
