bec926efdb314a55a3c6c018fa3c88f2b1d2c859
Katka Marcincakova <katulik137@gmail.com>
2023-05-16 19:10:23 +0200

    fe fixed

--- Haxee.Entities/Models/ChangeSettingsModel.cs
+++ Haxee.Entities/Models/ChangeSettingsModel.cs
@@ -3,12 +3,15 @@
     public class ChangeSettingsModel
     {
         [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Meno")]
         public string FirstName { get; set; } = string.Empty;
         
         [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Priezvisko")]
         public string LastName { get; set; } = string.Empty;
         
         [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
     }
